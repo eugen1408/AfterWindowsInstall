35b669404ed59b6a37819a25d9f0906f0f085b4b
eugen1408 <evaganov@outlook.com>
2019-08-21 15:06:09 +0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,3 @@
 #Requires -RunAsAdministrator
-Set-ExecutionPolicy Bypass
-cmd /c "bcdedit /set {default} bootmenupolicy legacy"
-Install-PackageProvider -Name chocolatey
-Install-PackageProvider -Name ChocolateyGet
-Set-PackageSource -Name chocolatey -trusted
+ls
+pause
