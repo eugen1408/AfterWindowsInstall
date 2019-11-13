dd53a7d8f19a2adf47915802361a5c09d19c2735
evaganov <evaganov@outlook.com>
2019-11-13 16:20:26 +0500

    Update choco.ps1

--- standard/choco.ps1
+++ standard/choco.ps1
@@ -2,7 +2,6 @@ Install-PackageProvider -Name ChocolateyGet -Force
 Set-PackageSource -Name chocolatey -Trusted -Force
 If ((Get-PnpDevice -ErrorAction SilentlyContinue -PresentOnly -Class System -InstanceId '*VEN_8086*' | Measure-Object).Count -gt 0) {Install-Package intel-chipset-device-software -Force}
 If ((Get-PnpDevice -ErrorAction SilentlyContinue -PresentOnly -Class Display -InstanceId '*VEN_10DE*' | Measure-Object).Count -gt 0) {Install-Package nvidia-display-driver -Force}
-Install-Package ChocolateyGui -Force
 Install-Package googlechrome -Force
 Install-Package 7zip -Force
 Install-Package qbittorrent -Force
