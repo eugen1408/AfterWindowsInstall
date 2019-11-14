6d733808feddf87cf461740eaaa57e7035d314ba
evaganov <evaganov@outlook.com>
2019-11-14 12:40:27 +0500

    Update choco.ps1

--- evaganov/choco.ps1
+++ evaganov/choco.ps1
@@ -1,5 +1,5 @@
 Install-Package everything -Force
-If ((Get-PnpDevice -ErrorAction SilentlyContinue -PresentOnly -Class Mouse -FriendlyName '*Logitech Gaming*' | Measure-Object).Count -gt 0) {Install-Package logitechgaming -Force}
-If ((Get-PnpDevice -ErrorAction SilentlyContinue -PresentOnly -Class Mouse -FriendlyName '*Unifying Mouse*' | Measure-Object).Count -gt 0) {Install-Package setpoint -Force}
+If ((Get-PnpDevice -ErrorAction SilentlyContinue -PresentOnly -Class Mouse -FriendlyName '*Logitech Gaming*' | Measure-Object).Count -gt 0) {Install-Package logitechgaming -Force -Force -ProviderName ChocolateyGet}
+If ((Get-PnpDevice -ErrorAction SilentlyContinue -PresentOnly -Class Mouse -FriendlyName '*Unifying Mouse*' | Measure-Object).Count -gt 0) {Install-Package setpoint -Force -Force -ProviderName ChocolateyGet}
 $confirmation = Read-Host "Install Media Tools? (y/n)"; if ($confirmation -eq 'y') { Install-Package foobar2000 -Force; Install-Package avisynthplus -Force }
 $confirmation = Read-Host "Install Development Tools? (y/n)"; if ($confirmation -eq 'y') { Install-Package vscode -Force -ProviderName ChocolateyGet; Install-Package azure-data-studio -Force; Install-Package github-desktop -Force; Install-Package postman -Force }
